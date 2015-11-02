558c5a86cf330cb030ab24d0c8c1ef3ea51fb121
thegergz <thegerg@live.com>
2015-11-02 01:54:25 -0500

    teest

--- app/src/main/java/com/bignerdranch/android/soonshoon/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/soonshoon/MainActivity.java
@@ -11,6 +11,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    //tacotest
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
